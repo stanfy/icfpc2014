0265de445959b31de6688ae0ea001cbd579e2df2
Paul Taykalo <tt.kilew@gmail.com>
2014-07-26 12:02:23 +0300

    CONS, CDR, CAR

--- src/main/java/com/stanfy/icfp2014/lambdaprocessor/instructions/ConsInstruction.java
+++ src/main/java/com/stanfy/icfp2014/lambdaprocessor/instructions/ConsInstruction.java
@@ -7,10 +7,15 @@ import com.stanfy.icfp2014.lambdaprocessor.LambdaManProcessor;
  * Created by ptaykalo on 7/26/14.
  */
 public class ConsInstruction implements LambdaManProcessorInstruction {
+//  $y,%s := POP(%s)
+//  $x,%s := POP(%s)
+//  $z := ALLOC_CONS($x,$y)
+//  %s := PUSH(SET_TAG(TAG_CONS,$z),%s)
+//  %c := %c+1
   @Override
   public void processOn(LambdaManProcessor processor) {
-    Integer y = (Integer) processor.popStackValue();
-    Integer x = (Integer) processor.popStackValue();
+    Object y =  processor.popStackValue();
+    Object x = processor.popStackValue();
     processor.pushStackValue(new Cons(x, y));
     processor.c +=1;
   }
