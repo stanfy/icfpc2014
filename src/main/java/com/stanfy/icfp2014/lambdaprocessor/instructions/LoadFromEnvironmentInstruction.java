afe256f9c53f16566fc9f43b07fa3faa049a2378
Paul Taykalo <tt.kilew@gmail.com>
2014-07-26 12:16:45 +0300

    LDC, LFE

--- src/main/java/com/stanfy/icfp2014/lambdaprocessor/instructions/LoadFromEnvironmentInstruction.java
+++ src/main/java/com/stanfy/icfp2014/lambdaprocessor/instructions/LoadFromEnvironmentInstruction.java
@@ -1,6 +1,7 @@
 package com.stanfy.icfp2014.lambdaprocessor.instructions;
 
 import com.stanfy.icfp2014.lambdaprocessor.EnvironmentFrame;
+import com.stanfy.icfp2014.lambdaprocessor.InstructionResult;
 import com.stanfy.icfp2014.lambdaprocessor.LambdaManProcessor;
 
 /**
@@ -16,21 +17,33 @@ public class LoadFromEnvironmentInstruction implements LambdaManProcessorInstruc
     this.variableIndex = variableIndex;
   }
 
+//  $fp := %e
+//  while $n > 0 do            ; follow chain of frames to get n'th frame
+//  begin
+//    $fp := FRAME_PARENT($fp)
+//    $n := $n-1
+//  end
+//  if FRAME_TAG($fp) == TAG_DUM then FAULT(FRAME_MISMATCH)
+//  $v := FRAME_VALUE($fp, $i) ; i'th element of frame
+//  %s := PUSH($v,%s)          ; push onto the data stack
+//  %c := %c+1
+
   @Override
-  public void processOn(LambdaManProcessor processor) {
+  public InstructionResult processOn(LambdaManProcessor processor) {
     EnvironmentFrame fp = processor.e;
     int n = frameIndex;
     while (n > 0)  {
-          // follow chain of frames to get n'th frame
       fp = fp.parent;
       n--;
     }
-
-    // null check
-   //    if FRAME_TAG($fp) == TAG_DUM then FAULT(FRAME_MISMATCH)
+    if (fp.isDummy) {
+      return InstructionResult.FAILURE_FRAME_MISMATCH;
+    }
     Object v = fp.items.get(variableIndex);
+
     processor.s.add(v);
     processor.c += 1;
+    return InstructionResult.SUCCESS;
   }
 
   @Override
