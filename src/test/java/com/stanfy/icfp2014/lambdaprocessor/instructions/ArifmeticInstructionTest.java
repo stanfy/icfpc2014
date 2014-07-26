9af3020ffc545abff554c231fa966235f377a592
Paul Taykalo <tt.kilew@gmail.com>
2014-07-26 04:56:59 +0300

    Mul and div instructions

--- src/test/java/com/stanfy/icfp2014/lambdaprocessor/instructions/ArifmeticInstructionTest.java
+++ src/test/java/com/stanfy/icfp2014/lambdaprocessor/instructions/ArifmeticInstructionTest.java
@@ -52,5 +52,43 @@ public class ArifmeticInstructionTest {
 
   }
 
+  @Test
+  public void testMultiplyingInstruction() throws Exception {
+    int counter = processor.c;
+
+    LoadConstantInstruction constant1 = LoadConstantInstruction.with(3);
+    LoadConstantInstruction constant2 = LoadConstantInstruction.with(4);
+    MulInstruction  addInstruction = new MulInstruction();
+
+    constant1.processOn(processor);
+    constant2.processOn(processor);
+
+    addInstruction.processOn(processor);
+
+    assertTrue(processor.c == counter + 3);
+    assertTrue(processor.topStackValue() == Integer.valueOf(12));
+
+
+  }
+
+  @Test
+  public void testDivInstruction() throws Exception {
+    int counter = processor.c;
+
+    LoadConstantInstruction constant1 = LoadConstantInstruction.with(10);
+    LoadConstantInstruction constant2 = LoadConstantInstruction.with(4);
+    DivInstruction  addInstruction = new DivInstruction();
+
+    constant1.processOn(processor);
+    constant2.processOn(processor);
+
+    addInstruction.processOn(processor);
+
+    assertTrue(processor.c == counter + 3);
+    assertTrue(processor.topStackValue() == Integer.valueOf(2));
+
+
+  }
+
 
 }
