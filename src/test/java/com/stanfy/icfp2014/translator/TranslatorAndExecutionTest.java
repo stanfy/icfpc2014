e71ca1b4d7e4006fd6ae7ab789fda62e91c1fa44
Paul Taykalo <tt.kilew@gmail.com>
2014-07-27 16:36:40 +0300

    updated test

--- src/test/java/com/stanfy/icfp2014/translator/TranslatorAndExecutionTest.java
+++ src/test/java/com/stanfy/icfp2014/translator/TranslatorAndExecutionTest.java
@@ -237,6 +237,26 @@ public class TranslatorAndExecutionTest {
     assertThat(processor.topStackValue().toString()).isEqualTo("((1, 2), (0, 0))");
   }
 
+  @Test
+  public void listLMap() {
+    LambdaManProcessor processor = processorWithLoadedProgram(
+"        (defn lMap [list f]"+
+"           (if (isInt list)"+
+"           nil"+
+"          (tuple ((f (first list)) (lMap (rest list) f)))"+
+"         ))"+
+""+
+"    (defn inc [x] (+ x 1))"+
+""+
+"    (defn main []"+
+"       (lMap (quote (1 2 3 nil)) inc)"+
+"    )"
+    );
+    processor.run();
+    assertThat(processor.topStackValue().toString()).isEqualTo("(2, (3, (4, 0)))");
+  }
+
+
   /*
 
 
